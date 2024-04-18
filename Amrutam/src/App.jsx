3ece397853ff26cc1060d5169b0a716789885985
Dev_Faisal <dfaisal59@gmail.com>
2024-04-18 08:44:12 +0530

    Fix responsive behavior in App.jsx

--- Amrutam/src/App.jsx
+++ Amrutam/src/App.jsx
@@ -3,16 +3,20 @@ import TopSection from './Components/TopSection'
 import Button from './Components/Button'
 import sc from './assets/social.svg'
 import HomePage from './Pages/HomePage'
+import './App.css'
 
 
 function App() {
+  if (window.innerWidth < 1403) {
+    return alert('The website is only for Desktop view. Please open it in Desktop view.')
+  }
+  else {
+    return (
+      <>
+        <HomePage />
+      </>
+    )
+  }
 
-
-  return (
-    <>
-      <HomePage />
-    </>
-  )
 }
-
 export default App
